2c6720f1e49e6802380f8afb01a06b2efe30f47f
Homer <honasanhomer@gmail.com>
2021-10-27 14:46:56 +0800

    3. Hide and Show button

--- app.js
+++ app.js
@@ -2,13 +2,28 @@ const app = Vue.createApp({
     data() {
         return{
             enteredGoal: '',
-            tasks: [],            
+            tasks: [],
+            active: true,
+            button: 'Hide',            
         }
       },
     methods: {
         addTasks(){
             this.tasks.push(this.enteredGoal)
         },
+        toggleBtn(){
+            if(this.enteredGoal === ''){
+                this.button = 'Hide';
+            }else{
+                this.active = !this.active;
+                if(this.active === true){
+                    this.button = 'Hide';
+                    this.active = 'hidden';
+                }else{
+                    this.button = 'Show List';
+                }
+            }
+        }
 
     },
 });
